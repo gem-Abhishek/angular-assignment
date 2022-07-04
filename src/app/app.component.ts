d2cbcba1568a9e231f5b84e539486f83104df050
gem-Abhishek <104361731+gem-Abhishek@users.noreply.github.com>
2022-07-04 11:35:23 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {
+  ColDef,
+  ColGroupDef,
+  GridApi,
+  GridReadyEvent,
+} from 'ag-grid-community';
+import frontendData from '../app/frontend.json';
+import { ProjectAllocationCustomisationComponent } from './project-allocation-customisation/project-allocation-customisation.component';
+import { SkillspopupComponent } from './skillspopup/skillspopup.component';
+
+interface Frontend{
+  Sno: Number;
+  Name:String;
+  Skills:String;
+  "Project Allocation":String;
+  Comment:String;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +25,93 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-assignment';
+  private gridApi!: GridApi;
+  columnApi:any;
+  public frontendColDefs: (ColDef | ColGroupDef)[]=[
+    {
+    headerName:'Frontend Table',
+    children:[
+    {headerName:"S.No",field:'Sno'},
+    {headerName:"Name",field:'Name'},
+    {headerName:"Skills",field:'Skills',cellRenderer:SkillspopupComponent},
+    {headerName:"Project Allocation",field:'Project Allocation',cellRenderer: ProjectAllocationCustomisationComponent},
+    {headerName:"Comment",field:'Comment',editable:true,
+  },
+    ],
+  },
+  ];
+  public defaultColDef: ColDef = {
+    enableRowGroup: true,
+    enablePivot: true,
+    enableValue: true,
+    sortable: true,
+    filter: true,
+    resizable: true,
+  };
+  public frontendRowData:Frontend[]=frontendData;
+  public domLayout: 'normal' | 'autoHeight' | 'print' = 'autoHeight';
+  public popupParent: HTMLElement = document.body;
+
+  OnGridReady(params: { api: any; columnApi: any; }){
+    this.gridApi=params.api;
+    this.columnApi=params.columnApi;
+  }
+
+  public BackendColDefs: (ColDef | ColGroupDef)[]=[
+    {
+    headerName:'Backend Table',
+    children:[
+    {headerName:"S.No",field:'Sno'},
+    {headerName:"Name",field:'Name'},
+    {headerName:"Skills",field:'Skills',cellRenderer:SkillspopupComponent},
+    {headerName:"Project Allocation",field:'Project Allocation',cellRenderer: ProjectAllocationCustomisationComponent},
+    {headerName:"Comment",field:'Comment', editable:true,
+  },
+    ],
+  },
+  ];
+  public BackendRowData:Frontend[]=frontendData;
+  // public projectAllocationColDefs: (ColDef | ColGroupDef)[]=[
+  //   {
+  //   headerName:'Project Allocation Table',
+  //   children:[
+  //   {headerName:"S.No",field:'Sno'},
+  //   {headerName:"Name",field:'Name'},
+  //   {headerName:"Skills",field:'Skills'},
+  //   {headerName:"Project Allocation",field:'Project Allocation'},
+  //   {headerName:"Comment",field:'Comment',editable:true,
+  // },
+  //   ],
+  // },
+  // ];
+  // public projectAllocationRowData:Frontend[]=[];
+  
+  receiveMessage($event: any){
+    console.log($event);
+  }
+  
+  getRowId=(params: { data: { Sno: any; }; })=>params.data.Sno;
+
+  onRemoveSelected() {
+    var selectedRowData = this.gridApi.getSelectedRows();
+    this.gridApi.applyTransaction({ remove: selectedRowData });
+  }
+
+  
+
+  parentFunction(data: any){
+    console.log(data);
+    console.log("%%%");
+  }
+
+  public rowSelection='single';
+  onSelectionChanged() {
+    const selectedRows = this.gridApi.getSelectedRows();
+    console.log(this.gridApi.getSelectedRows());
+  }
 }
+
+
+  
+  
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AgGridModule } from 'ag-grid-angular';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectAllocationCustomisationComponent } from './project-allocation-customisation/project-allocation-customisation.component';
+import { SkillspopupComponent } from './skillspopup/skillspopup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectAllocationCustomisationComponent,
+    SkillspopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    AgGridModule,
   ],
-  providers: [],
+  providers: [], entryComponents:[ProjectAllocationCustomisationComponent,SkillspopupComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
